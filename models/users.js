860c76c68f5f5c0bb9de891dff0860ffec2ff250
ericadiasdematos <erica.diasmatos99@gmail.com>
2020-10-09 11:21:36 +0200

    link to bdd

--- models/users.js
+++ models/users.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose')
 
+const likedArticlesSchema = mongoose.Schema({
+    title: String,
+    desc: String,
+    content: String,
+    image: String,
+    link: String,
+})
+
 const userSchema = mongoose.Schema({
     username: String,
     email: String,
     password: String,
     token: String,
     salt: String, 
+    likedArticles: likedArticlesSchema,
+
 })
 
 const userModel = mongoose.model('users', userSchema)
